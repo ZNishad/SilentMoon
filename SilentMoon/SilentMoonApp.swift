39eaa83bb1b95c24ae34c10738b799863fdc8e47
ZNishad <zulfuqarlinishat@gmail.com>
2026-07-02 15:49:22 +0400

    feat: initial project setup

---
+++ SilentMoon/SilentMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilentMoonApp.swift
+//  SilentMoon
+//
+//  Created by Nishad Zulfuqarli on 02.07.26.
+//
+
+import SwiftUI
+
+@main
+struct SilentMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SilentMoonTests/SilentMoonTests.swift
@@ -0,0 +1,19 @@
+//
+//  SilentMoonTests.swift
+//  SilentMoonTests
+//
+//  Created by Nishad Zulfuqarli on 02.07.26.
+//
+
+import Testing
+@testable import SilentMoon
+
+struct SilentMoonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
